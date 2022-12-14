4042f4a12ea18eb5bd661c6b100ad5eda3854050
Jesse Kuntz <jesseckuntz@gmail.com>
2022-12-13 19:03:22 -0800

    Create segmented controls

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
+import { useRef, useState, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import  SegmentedControl from './SegmentedControl';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your sapp!</Text>
+      <SegmentedControl
+        name="group-1"
+        callback={(val) => console.log(val)}
+        defaultIndex={0}
+        controlRef={useRef()}
+        segments={[
+          {
+            label: 'Complete',
+            value: 'complete',
+            ref: useRef(),
+          },
+          {
+            label: 'Incomplete',
+            value: 'incomplete',
+            ref: useRef(),
+          },
+          {
+            label: 'Pending',
+            value: 'pending',
+            ref: useRef(),
+          }
+        ]}
+      />
       <StatusBar style="auto" />
     </View>
   );
