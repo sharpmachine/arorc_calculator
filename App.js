aa6f76cf027f8c1d8e2799821bcb0ca52a8a02d9
Jesse Kuntz <jesseckuntz@gmail.com>
2023-01-02 19:44:09 -0800

    Move segmented control segment prop array to a constant

--- App.js
+++ App.js
@@ -8,51 +8,91 @@ import good from './assets/good.png';
 import bad from './assets/bad.png';
 
 export default function App() {
-  const showOutput = false;
+  const showOutput = true;
   
   const [trade, setTrade] = useState({
     type: 'BPS',
-    spread: 5,
+    spread: 1,
     strike: 1.08,
     costBasis: 65,
     dte: 28,
     credit: 0.19
   });
 
-  const [arorc, setArorc] = useState({arorc: 20, rorc: 20, minCredit: 1.09});
+  const [tradeReturns, setArorc] = useState({
+    arorc: 20, 
+    rorc: 20, 
+    minCredit: 1.09
+  });
 
-  const onValueChange = (name, value, index) => {
+  const spreads = [
+    {
+      label: '$1',
+      value: 1
+    },
+    {
+      label: '$5',
+      value: 5
+    },
+    {
+      label: '$10',
+      value: 10
+    },
+    {
+      label: '$20',
+      value: 20
+    }
+  ]
+
+  const trades = [
+    {
+      label: 'BPS',
+      value: 'BPS'
+    },
+    {
+      label: 'BCS',
+      value: 'BCS'
+    },
+    {
+      label: 'ROP',
+      value: 'ROP'
+    },
+    {
+      label: 'ROC',
+      value: 'ROC'
+    }
+  ]
+
+  const onValueChange = (name, value) => {
     setTrade({
       ...trade,
       [name]: value
     }, calculateArorc());
   }
 
-  const tradeTypes = [
-    "BPS",
-    "BCS",
-    "ROP",
-    "ROC"
-  ]
+  const meetsMarr = ()=> {
+    if (trade.type == "BPS" || trade.type == "BCS") {
+      if (tradeReturns.arorc >= 48) {
+        return true
+      } else return false
+    } else {
+      if (tradeReturns.arorc >= 20) {
+        return true
+      } else return false
+    }
+  }
 
-  const spreads = [1, 5, 10, 20]
+  
 
-  // const {
-  //   type,
-  //   spread,
-  //   strike,
-  //   costBasis,
-  //   dte,
-  //   credit
-  // } = trade
 
-  const calculateArorc = () => {
+
+  function calculateArorc() {
     let riskCapital = trade.spread - trade.credit; // TODO: RC formula varies based on trade type
     let returnOnRiskCapital = trade.credit / riskCapital;
     let multiplier = 365 / trade.dte;
     let annualizedReturnOnRiskCapital = (returnOnRiskCapital * multiplier) * 100;
     //TODO: calculate min credit
-    setArorc({arorc: annualizedReturnOnRiskCapital, rorc: returnOnRiskCapital, minCredit: 1.09})
+    setArorc({ arorc: annualizedReturnOnRiskCapital, rorc: returnOnRiskCapital, minCredit: 1.09 });
   }
 
   const ShowBaseField = () => {
@@ -60,31 +100,9 @@ export default function App() {
       return <SegmentedControl
       name="spread"
       callback={onValueChange}
-      defaultIndex={spreads.indexOf(trade.spread)}
-      // controlRef={useRef()}
+      defaultIndex={spreads.findIndex(s => s.value === trade.spread)}
       label="Spread"
-      segments={[
-        {
-          label: '$1',
-          value: 1,
-          // ref: useRef(),
-        },
-        {
-          label: '$5',
-          value: 5,
-          // ref: useRef(),
-        },
-        {
-          label: '$10',
-          value: 10,
-          // ref: useRef(),
-        },
-        {
-          label: '$20',
-          value: 20,
-          // ref: useRef(),
-        }
-      ]}
+      segments={spreads}
     />
 
     } else if (trade.type == 'ROP') {
@@ -93,7 +111,6 @@ export default function App() {
       defaultValue={trade.strike}
       showCurrencySign={true}
       callback={onValueChange}
-      // controlRef={useRef()}
       label="Strike Price"
     />
     } else {
@@ -104,7 +121,6 @@ export default function App() {
       showNudgers={false}
       showSlider={false}
       callback={onValueChange}
-      // controlRef={useRef()}
       label="Cost Basis"
     />
     }
@@ -120,31 +136,9 @@ export default function App() {
       <SegmentedControl
         name="type"
         callback={onValueChange}
-        defaultIndex={tradeTypes.indexOf(trade.type)}
-        controlRef={useRef()}
+        defaultIndex={trades.findIndex(t => t.value === trade.type)}
         label="Trade"
-        segments={[
-          {
-            label: 'BPS',
-            value: 'BPS',
-            ref: useRef(),
-          },
-          {
-            label: 'BCS',
-            value: 'BCS',
-            ref: useRef(),
-          },
-          {
-            label: 'ROP',
-            value: 'ROP',
-            ref: useRef(),
-          },
-          {
-            label: 'ROC',
-            value: 'ROC',
-            ref: useRef(),
-          }
-        ]}
+        segments={trades}
       />
 
       {ShowBaseField()}
@@ -164,19 +158,19 @@ export default function App() {
         showCurrencySign={true}
         controlRef={useRef()}
         label="Credit"
-        minCredit={arorc.minCredit}
+        minCredit={tradeReturns.minCredit}
       />
       {showOutput &&
         <div>
           <div>{JSON.stringify(trade, null, 2)}</div>
-          <div>{JSON.stringify(arorc, null, 2)}</div> 
+          <div>{JSON.stringify(tradeReturns, null, 2)}</div> 
         </div>
       }
 
       <div className="result">
         <div className='result-arorc'>
-          <img src={trade.type == "BPS" ? good : bad} alt="" width="32" />
-          <span className='result-percentage'>{Math.floor(arorc.arorc)}%</span>
+          <img src={meetsMarr() ? good : bad} alt="" width="32" />
+          <span className='result-percentage'>{Math.floor(tradeReturns.arorc)}%</span>
           <span className='result-label'>ARORC</span>
         </div>
         <div className='result-marr'>
